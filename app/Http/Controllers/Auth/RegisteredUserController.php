9d20daad9017e661f7864867b4819b92a5bb6b0c
Muhamad Hudansah <hudansah0@gmail.com>
2024-02-19 19:41:19 +0700

    update 7

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,39 +29,76 @@ class RegisteredUserController extends Controller
      * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): RedirectResponse
-    {
+{
+    $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+        'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        'role' => ['required', 'in:user,TOKO,admin'],
+        'address' => ($request->role == 'user') ? [] : ['required', 'string', 'max:255'],
+        'image' => ($request->role == 'user') ? [] : ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+    ]);
+
+    $imagePath = null;
+
+    if ($request->hasFile('image') && $request->role !== 'user') {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'role' => ['required', 'in:user,TOKO,admin'],
-            'address' => ['required', 'string', 'max:255'],
             'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
         ]);
-        $imagePath = null;
 
-        if ($request->hasFile('image')) {
-            $request->validate([
-                'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
-            ]);
+        $imagePath = $request->file('image')->store('uploads', 'public');
+    }
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'address' => ($request->role == 'user') ? null : $request->address,
+        'password' => Hash::make($request->password),
+        'role' => $request->role,
+        'image' => ($request->role == 'user') ? null : $imagePath,
+    ]);
+
+    event(new Registered($user));
+
+    Auth::login($user);
+
+    return redirect(RouteServiceProvider::HOME);
+}
+
+    // public function store(Request $request): RedirectResponse
+    // {
+    //     $request->validate([
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+    //         'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    //         'role' => ['required', 'in:user,TOKO,admin'],
+    //         'address' => ['required', 'string', 'max:255'],
+    //         'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+    //     ]);
+    //     $imagePath = null;
+
+    //     if ($request->hasFile('image')) {
+    //         $request->validate([
+    //             'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+    //         ]);
         
-            $imagePath = $request->file('image')->store('uploads', 'public');
-        }
+    //         $imagePath = $request->file('image')->store('uploads', 'public');
+    //     }
     
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'address' => $request->address,
-            'password' => Hash::make($request->password),
-            'role' => $request->role,
-            'image' => $imagePath,
-        ]);
+    //     $user = User::create([
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'address' => $request->address,
+    //         'password' => Hash::make($request->password),
+    //         'role' => $request->role,
+    //         'image' => $imagePath,
+    //     ]);
 
-        event(new Registered($user));
+    //     event(new Registered($user));
 
-        Auth::login($user);
+    //     Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
-    }
+    //     return redirect(RouteServiceProvider::HOME);
+    // }
 }
